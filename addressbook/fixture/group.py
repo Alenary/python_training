9fa8c26c22ba9f5b7be85178210f0afb84a83f92
Александра Гольцова <aleksandragolcova@MacBook-Pro-Aleksandra.local>
2021-02-25 00:26:14 +0300

    updated lesson 2 task 7

--- addressbook/fixture/group.py
+++ addressbook/fixture/group.py
@@ -11,15 +11,7 @@ class GroupHelper:
         self.open_groups_page()
         wd.find_element_by_name("new").click()  # создание новой группы
         # заполнение группы
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.group_name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_info(group, wd)
         wd.find_element_by_name("submit").click()  # сохранение группы
         self.return_to_group_page()
 
@@ -39,12 +31,15 @@ class GroupHelper:
         self.open_groups_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
+        self.change_info(group, wd)
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+
+    def change_info(self, group, wd):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").send_keys(group.group_name)  # добавление текста к существующему
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)   # полная замена текста
+        wd.find_element_by_name("group_header").send_keys(group.header)  # полная замена текста
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("update").click()
-        self.return_to_group_page()
